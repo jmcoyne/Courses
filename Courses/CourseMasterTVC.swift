2f13c3baf6cc67cc79ec232936552fcf111f1c78
jmcoyne <jcoyne@mzinga.com>
2014-12-16 13:14:05 -0500

    Initial commit

--- Courses/CourseMasterTVC.swift
+++ Courses/CourseMasterTVC.swift
@@ -105,14 +105,29 @@ class CourseMasterTVC: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
-    }
-    */
+        
+            if segue.identifier == "CourseDetail" {
+                let indexPath = self.tableView.indexPathForSelectedRow()
+                NSLog("Seguing to CourseDetail")
+                var course : Course
+                
+                course  = courses[indexPath!.row]
+                    let courseDetailVC = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
+                    courseDetailVC.title = "class"
+                //}
+        
+                
+           
+            
+        }
 
+    }
+    
 }
