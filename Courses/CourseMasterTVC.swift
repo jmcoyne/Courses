dd692385bc6673c78255e8ffef3a793bc5139b8b
Joan Coyne <joan@jcoyne.net>
2014-12-30 17:19:07 -0500

    Started adding in more view controller code on the main VC

--- Courses/CourseMasterTVC.swift
+++ Courses/CourseMasterTVC.swift
@@ -16,7 +16,11 @@ class CourseMasterTVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let split = self.splitViewController {
+            let controllers = split.viewControllers
+            self.detailViewController = controllers.last?.topViewController as? DetailViewController
         
+        }
         
 
         // Uncomment the following line to preserve selection between presentations
@@ -61,19 +65,10 @@ class CourseMasterTVC: UITableViewController {
                 
             }
             
-                       
+            
         }
 
-        
-      /*  self.courses = [Course (name:"Great Class", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc lacus sapien, commodo vestibulum luctus quis, porttitor ac felis. Cras fermentum mi arcu, non vehicula dolor pharetra vitae. Donec vitae leo quis dolor feugiat tempor aliquam cursus est. Morbi quis posuere urna, ut pretium nulla. In velit dui, rutrum vitae laoreet a, blandit tempor dui. Donec sollicitudin accumsan lacinia. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;",  imageURL:"mzingabeeCloseup.jpg", createdAt:"Last Week:",   updatedAt:"Last Week:", commentsCount:"5", sectionId:"1"),
-            Course(name:"Great 2 Class", description:"You hit me with a cricket bat. The way I see it, every life is a pile of good things and bad things.…hey.…the good things don't always soften the bad things; but vice-versa the bad things don't necessarily spoil the good things and make them unimportant. Stop talking, brain thinking. Hush. It's a fez. I wear a fez now. Fezes are cool. ",  imageURL:"IMG_0187.jpg", createdAt:"Yesterday",  updatedAt:"Today", commentsCount:"3", sectionId:"2"),
-            Course(name:"Great 3 Class", description:"Dantooine. They're on Dantooine. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers. The plans you refer to will soon be back in our hands. Partially, but it also obeys your commands. Look, I can take you as far as Anchorhead. You can get a transport there to Mos Eisley or wherever you're going. A tremor in the Force. The last time I felt it was in the presence of my old master.",  imageURL:"IMG_0140.jpg", createdAt:"Last Week:", updatedAt:"Last Week:", commentsCount:"8", sectionId:"3"),
-            Course(name:"Great 4 Class", description:"Leave that to me. Send a distress signal, and inform the Senate that all on board were killed. Don't be too proud of this technological terror you've constructed. The ability to destroy a planet is insignificant next to the power of the Force. But with the blast shield down, I can't even see! How am I supposed to fight?",  imageURL:"IMG_0201.jpg", createdAt:"Last Week:", updatedAt:"Last Week:", commentsCount:"5", sectionId:"1"),
-            Course(name:"Great 5 Class", description:"Dantooine. They're on Dantooine. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers. The plans you refer to will soon be back in our hands. Partially, but it also obeys your commands. Look, I can take you as far as Anchorhead. You can get a transport there to Mos Eisley or wherever you're going. A tremor in the Force. The last time I felt it was in the presence of my old master.",  imageURL:"IMG_0223.jpg", createdAt:"Last Week:", updatedAt:"Last Week:", commentsCount:"5", sectionId:"2"),
-            Course(name:"Great 6 Class", description:"Leave that to me. Send a distress signal, and inform the Senate that all on board were killed. Don't be too proud of this technological terror you've constructed. The ability to destroy a planet is insignificant next to the power of the Force. But with the blast shield down, I can't even see! How am I supposed to fight?",  imageURL:"IMG_0561.jpg", createdAt:"Last Week:", updatedAt:"Last Week:", commentsCount:"5", sectionId:"3"),
-            Course(name:"Great 7 Class", description:"You hit me with a cricket bat. The way I see it, every life is a pile of good things and bad things.…hey.…the good things don't always soften the bad things; but vice-versa the bad things don't necessarily spoil the good things and make them unimportant. Stop talking, brain thinking. Hush. It's a fez. I wear a fez now. Fezes are cool.",  imageURL:"IMG_0021.jpg", createdAt:"Last Week:",   updatedAt:"Last Week:", commentsCount:"5", sectionId:"1"),
-            Course(name:"Great 8 Class", description:"Dantooine. They're on Dantooine. The more you tighten your grip, Tarkin, the more star systems will slip through your fingers. The plans you refer to will soon be back in our hands. Partially, but it also obeys your commands. Look, I can take you as far as Anchorhead. You can get a transport there to Mos Eisley or wherever you're going. A tremor in the Force. The last time I felt it was in the presence of my old master.",  imageURL:"IMG_0095.jpg", createdAt:"Last Week:",  updatedAt:"Last Week:", commentsCount:"5", sectionId:"2"),
-            Course(name:"Great 9 Class", description:"Leave that to me. Send a distress signal, and inform the Senate that all on board were killed. Don't be too proud of this technological terror you've constructed. The ability to destroy a planet is insignificant next to the power of the Force. But with the blast shield down, I can't even see! How am I supposed to fight?",  imageURL:"IMG_0204.jpg", createdAt:"Last Week:",updatedAt:"Last Week:", commentsCount:"5", sectionId:"3")] */
+     
         
         self.tableView.reloadData()
     }
@@ -111,7 +106,17 @@ class CourseMasterTVC: UITableViewController {
     }
     
    
-    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if (UIDevice.currentDevice().userInterfaceIdiom == .Pad) {
+            // Check to see if there are any child classes in this section.  IF there are, show the indicator
+            //if rowHasChildrenAtIndex(indexPath) {
+                cell.accessoryType = .DisclosureIndicator
+            //} else{
+              //  cell.accessoryType = .None
+            //}
+            
+        }
+    }
    
 
     /*
